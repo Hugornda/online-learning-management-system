ddb84662e276b9a5dbe801782ab805a05d15a78d
hugornda <hugornda@gmail.com>
2024-10-02 15:10:43 +0100

    feat: added first files to the user management micro-service

---
+++ user-mgmt-ms/src/main/java/com/github/user_mgmt_ms/UserMgmtMsApplication.java
@@ -0,0 +1,13 @@
+package com.github.user_mgmt_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMgmtMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMgmtMsApplication.class, args);
+	}
+
+}
---
+++ user-mgmt-ms/src/test/java/com/github/user_mgmt_ms/UserMgmtMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.user_mgmt_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMgmtMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
